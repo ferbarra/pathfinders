2ebe28e9d2def9b7ae5d6d9627787287fe615257
Fernando Barra <fernandobarra2207@gmail.com>
2017-05-06 01:00:27 -0400

    Why-us slider made mobile friendlY

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,26 @@
 $(document).ready(function() {
     $("#why-us .slider").slick({
         autoplay: false,
-        slidesToShow: 3
+        slidesToShow: 3,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 830,
+                settings: {
+                    centerMode: true,
+                    speed: 500,
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 450,
+                settings: {
+                    slidesToShow: 1,
+                    autoplay: true,
+                    speed: 500,
+                    arrows: false
+                }
+            }
+        ],
     })
 })
