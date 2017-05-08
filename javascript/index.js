13dcaf675b1627a620666eb0c0d0f6eccb7f81b3
Fernando Barra <fernandobarra2207@gmail.com>
2017-05-07 23:25:34 -0400

    Website completed

--- javascript/index.js
+++ javascript/index.js
@@ -22,5 +22,29 @@ $(document).ready(function() {
                 }
             }
         ],
-    })
-})
+    });
+});
+
+$(window).on('resize', function() {
+  var win = $(this);
+  if (win.width() <= 765) {
+      $('#customer-service').removeClass('five');
+      $('#customer-service').removeClass('columns');
+      $('#customer-service').removeClass('offset-by-one');
+      $('#technical-assistance').removeClass('five');
+      $('#technical-assistance').removeClass('columns');
+      
+      $('#customer-service').addClass('u-full-width');
+      $('#technical-assistance').addClass('u-full-width');
+
+  } else {
+    $('#customer-service').removeClass('u-full-width');
+    $('#technical-assistance').removeClass('u-full-width');
+      
+    $('#customer-service').addClass('five');
+    $('#customer-service').addClass('columns');
+    $('#customer-service').addClass('offset-by-one');
+    $('#technical-assistance').addClass('five');
+    $('#technical-assistance').addClass('columns');
+  }
+});
